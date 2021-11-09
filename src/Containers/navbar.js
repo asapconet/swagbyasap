5bc4274fb8edf115563b5d49fd4c4d884960b0a3
asapconet <aaronrickymeek@gmail.com>
2021-11-09 04:14:18 +0100

    initailizations

---
+++ src/Containers/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+      <nav>
+        <img src="" alt="" />
+        <div>
+          <p>company</p>
+          <p>education</p>
+          <p>free leaning</p>
+          <p>pricing</p>
+          <p>contanct</p>
+        </div>
+        <div>
+            <input></input>
+            <button>signin</button>
+            <button>signup</button>
+        </div>
+      </nav>
+    );
+}
+
+export default navbar
