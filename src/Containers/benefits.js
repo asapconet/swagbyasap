490d792dd1ea618c307df6e465ca6c6257f866bc
asapconet <aaronrickymeek@gmail.com>
2021-11-13 18:41:54 +0100

    benifit section ongoing

--- src/Containers/benefits.js
+++ src/Containers/benefits.js
@@ -2,10 +2,54 @@ import React from 'react'
 
 const Benefits = () => {
     return (
-        <section>
-            
-        </section>
-    )
+      <section className="benefit-box">
+        <div className="top-box">
+          <div className="main-box">
+            <h1>Benefits</h1>
+            <p>
+              The Swag Academy is the home of the #1 FOREX YouTube channel in
+              the world. When we say “Day Trading Forex Simplified,” we don’t
+              mean trading in the market is easy. We mean, we break down a very
+              complex market in a way where it’s easy to grasp and understand!
+              Whether you’re a beginner or an advanced trader, there’s always
+              something new you can learn. Our valued educational course is
+              jam-packed with an infinite value. Here are some of the benefits
+              of joining:
+            </p>
+          </div>
+        </div>
+        <div className="sub-container">
+          <div className="sub-boxes">
+            <img src="" alt="benefit-icons" />
+            <h3>Everything Forex</h3>
+            <p>
+              Everything we believe you need to understand Forex is in here! If
+              by some chance it's not, we have a suggestion box where you can
+              submit anything we may have missed that you’d like to see.
+            </p>
+          </div>
+          <div className="sub-boxes">
+            <img src="" alt="benefit-icons" />
+            <h3>Everything Forex</h3>
+            <p>
+              Everything we believe you need to understand Forex is in here! If
+              by some chance it's not, we have a suggestion box where you can
+              submit anything we may have missed that you’d like to see.
+            </p>
+          </div>
+          <div className="sub-boxes">
+            <img src="" alt="benefit-icons" />
+            <h3>Everything Forex</h3>
+            <p>
+              Everything we believe you need to understand Forex is in here! If
+              by some chance it's not, we have a suggestion box where you can
+              submit anything we may have missed that you’d like to see.
+            </p>
+          </div>
+          
+        </div>
+      </section>
+    );
 }
 
 export default Benefits
