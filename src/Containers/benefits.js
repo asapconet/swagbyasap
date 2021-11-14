769a33ebf94a06342cb089740de34741a7f1eb25
asapconet <aaronrickymeek@gmail.com>
2021-11-14 18:06:38 +0100

    completion for benifit section- hard-code data

--- src/Containers/benefits.js
+++ src/Containers/benefits.js
@@ -30,23 +30,53 @@ const Benefits = () => {
           </div>
           <div className="sub-boxes">
             <img src="" alt="benefit-icons" />
-            <h3>Everything Forex</h3>
+            <h3>130+ Videos, PDF’s + Quizzes</h3>
             <p>
-              Everything we believe you need to understand Forex is in here! If
-              by some chance it's not, we have a suggestion box where you can
-              submit anything we may have missed that you’d like to see.
+              The videos, pdf's and quizzes are fun, clever, and light-hearted
+              as we try our best not to make them boring and monotonous.​
+              Similar to our YouTube channel, we try to entertain & educate!
             </p>
           </div>
           <div className="sub-boxes">
             <img src="" alt="benefit-icons" />
-            <h3>Everything Forex</h3>
+            <h3>Unreal Experience</h3>
             <p>
-              Everything we believe you need to understand Forex is in here! If
-              by some chance it's not, we have a suggestion box where you can
-              submit anything we may have missed that you’d like to see.
+              This is not just a basic course. It's an unparalleled EXPERIENCE.
+              Simply just having videos on a website is easy. It's different
+              when you impact people with a once in a lifetime experience. You
+              won't get this any where else.
             </p>
           </div>
-          
+        </div>
+        <div className="sub-container">
+          <div className="sub-boxes">
+            <img src="" alt="benefit-icons" />
+            <h3>Refund Policy</h3>
+            <p>
+              Although it is digital content and 100% of colleges and 99% of
+              those selling an online program don’t offer refunds, we’re proud
+              to give you a refund if you meet our refund policy.
+            </p>
+          </div>
+          <div className="sub-boxes">
+            <img src="" alt="benefit-icons" />
+            <h3>Trading Element</h3>
+            <p>
+              We really TRADE here! A lot of people just give signals or post
+              MT4 screenshots after their winning trades and that's it. We
+              believe a walkthrough process for wins AND losses is necessary for
+              growth!
+            </p>
+          </div>
+          <div className="sub-boxes">
+            <img src="" alt="benefit-icons" />
+            <h3>Swaggy C</h3>
+            <p>
+              There’s a reason you all gravitate towards me first on YouTube
+              over any other YouTuber. No other course has ME. You won’t regret
+              it!​
+            </p>
+          </div>
         </div>
       </section>
     );
