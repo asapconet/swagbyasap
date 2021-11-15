ae8f148a94118a575dc982e771969214ceca3de4
asapconet <aaronrickymeek@gmail.com>
2021-11-15 04:41:03 +0100

    visualization of testimonial section

--- src/Containers/testimonials.js
+++ src/Containers/testimonials.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Button from "../Components/buttons";
 
 const Testimonials = () => {
-    return (
-        <div>
-            
+  return (
+    <section className="testimonial">
+      <div className="gen-body">
+        <div className="gen-head">
+          <h4>Member Testimonials</h4>
+          <h3>What users say about our platform</h3>
         </div>
-    )
-}
+        <div className="disclaimer">
+          The members posting their reviews are of their own opinion and not
+          made in consultation with anyone at The Swag Academy. Anyone posting
+          about having specific results within The Academy have not been
+          personally verified by The Swag Academy. Any comments that you see are
+          their personal opinion and we are not in partnership with them
+          whatsoever.
+        </div>
+      </div>
+      <div className="slide-box">
+        <div className="slide-container"></div>
+        <div className="btn-box">
+          <Button className="last-btn">Register Now --</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
